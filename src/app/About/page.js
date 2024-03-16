b001d068512783d3745a6f45424b74ebbe0c186f
Tony Fleming <tony@maetek.com>
2024-03-16 19:51:01 -0400

    Change about page

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const About = () => {
   return (
-    <div>
+    <Container>
       <h1>About</h1>
-    </div>
+    </Container>
   )
 }
 
